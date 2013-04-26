8fa36b577cef32338e5cd0b638586f5e4ec845b6
Qanthelas <qanthelas@gmail.com>
2013-04-26 10:12:19 -0400

    Add in the first round of test files

---
+++ main.py
@@ -0,0 +1,66 @@
+import kivy
+kivy.require('1.5.1')
+
+import gettext
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+#from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
+import os
+
+# Set up message catalog access
+dir = os.path.dirname(__file__)
+languagePath = os.path.join(dir, 'language')
+gettext.bindtextdomain('multilingual', languagePath)
+
+class Multilingual(BoxLayout):
+    '''Basics for widget where we select and then test a language preference.
+    '''
+    label_language = ObjectProperty()
+    greeting = StringProperty()
+    testtext = StringProperty()
+
+    def set_language(self,selectedLanguage):
+        self.t = gettext.translation('multilingual', languagePath, languages=[selectedLanguage], fallback=True)
+        _ = self.t.ugettext #The 'u' in 'ugettext' is for Unicode - use this to keep Unicode from breaking the app
+        self.greeting = _('Hello!')
+        #return _
+    
+    def myfunction(self, text):
+        self.result = text + ' this is a test'
+        #self.testtext = self.result
+        return self.result
+
+
+
+class MultilingualApp(App):
+    def build(self):
+        self.root = Multilingual(greeting='Translated Message Will Be Here')
+        self.root.set_language('en_US')
+        #self.root.myfunction('blah')
+      
+    def build_config(self, config):
+        config.add_section('localization')
+        config.set('localization', 'language', 'en_US')
+    
+    def build_settings(self, settings):
+        settings.add_json_panel('Multilingual', self.config, data='''[
+            { "type": "title", "title": "Language Settings" },
+            { "type": "options", "title": "Language",
+              "desc": "Choose which language to translate the text of this app into",
+              "section": "localization", "key": "language",
+              "options": ["en_US", "de", "fr", "es"]}
+        ]''')
+
+    def on_config_change(self, config, section, key, value):
+        if config is not self.config:
+            return
+        token = (section, key)
+        if token == ('localization', 'language'):
+            self.root.set_language(value)
+        print "Language is now", value
+
+if __name__ == '__main__':
+    MultilingualApp().run()
